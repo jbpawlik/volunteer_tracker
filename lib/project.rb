dc1c62c149747c35a29ac3350c2a2e16f7f7d1a9
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-30 08:46:39 -0700

    finds project in database; spec passing

--- lib/project.rb
+++ lib/project.rb
@@ -27,4 +27,15 @@ class Project
     projects
   end
 
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    if project
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      Project.new({:title => title, :id => id})
+    else
+      nil
+    end
+  end
+
 end
