3a7f60c3fd41be6d0051580a5e5b00c216e2dfb1
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-30 08:34:47 -0700

    add Project class; initialize spec passing

--- lib/project.rb
+++ lib/project.rb
@@ -2,9 +2,9 @@ class Project
   attr_reader :id
   attr_accessor :title 
 
-  def initialize(title)
-    @title = title
-    @id = id
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
   end
 
 end
