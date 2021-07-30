ac403e8226c8940a1b82712942dbf20d1508dc15
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-30 10:35:32 -0700

    create new project; int spec passing

--- spec/project_integration_spec.rb
+++ spec/project_integration_spec.rb
@@ -7,7 +7,7 @@ require('spec_helper')
 describe('create a project path', {:type => :feature}) do
   it('creates a project and then goes to the project page') do
     visit('/projects')
-    click_on('Add a new project')
+    click_on('add a new project')
     fill_in('project_title', :with => 'Stink Removal')
     click_on('Add project')
     expect(page).to have_content('Stink Removal')
@@ -18,7 +18,7 @@ describe('update a project title', {:type => :feature}) do
   it('goes to the edit project page and enters a new title') do
     visit('/')
     visit('/projects')
-    click_on('Add a new project')
+    click_on('add a new project')
     fill_in('project_title', :with => 'Stink Removal')
     click_on('Add project')
     click_on('Stink Removal')
@@ -33,7 +33,7 @@ describe('delete a project', {:type => :feature}) do
   it('goes to the edit page and deletes the project') do
     visit('/')
     visit('/projects')
-    click_on('Add a new project')
+    click_on('add a new project')
     fill_in('project_title', :with => 'Stink Removal')
     click_on('Add project')
     click_on('Stink Removal')
